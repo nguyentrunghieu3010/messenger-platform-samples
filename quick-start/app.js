f9ecd4ba29360bdf898504162206c314bb75365a
hieu nguyen <nthieu30101985@gmail.com>
2018-03-27 20:58:00 +0700

    update

--- quick-start/app.js
+++ quick-start/app.js
@@ -21,19 +21,21 @@
  */
 
 'use strict';
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN || "EAALQZBQJOZCFYBAI0RZC2ZAkOXYXzydaJAOvZBeSBY9C0eISz1yWLTu0rZBDUCOeRaWVQgUmIntxmXUsifCcQO6oNm50gTTwSiINSnILR2ZAgdjV4n81okp2ZAXaXjkE6JZAl7hVSqHVatDmMZCKEQhY05tTqqdTl9XyesfruqqJFpUwZDZD";
 // Imports dependencies and set up http server
-const 
+const
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
+app.set('port', (process.env.PORT || 5000))
+
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(app.get('port'), () => console.log('webhook is listening', app.get('port')));
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
+app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
@@ -41,26 +43,24 @@ app.post('/webhook', (req, res) => {
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    body.entry.forEach(function(entry) {
+    body.entry.forEach(function (entry) {
 
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
 
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
+        handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
-        
+
         handlePostback(sender_psid, webhook_event.postback);
       }
-      
+
     });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
@@ -74,42 +74,73 @@ app.post('/webhook', (req, res) => {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
+
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
-  
+  const VERIFY_TOKEN = "ChatbotMitsubishi";
+
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Check if a token and mode were sent
   if (mode && token) {
-  
+
     // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
+  } else {
+    console.log("Error: mode & token", mode, token);
+    res.sendStatus(302);
   }
 });
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  
+
+
+
+
   // Checks if the message contains text
-  if (received_message.text) {    
+  if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
+    console.log('received_message.text', received_message.text);
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
+    // response = {
+    //   "attachment": {
+    //     "type": "template",
+    //     "payload": {
+    //       "template_type": "generic",
+    //       "elements": [{
+    //         "title": "Is this the right picture?",
+    //         "subtitle": "Tap a button to answer.",
+    //         // "image_url": attachment_url,
+    //         "buttons": [
+    //           {
+    //             "type": "postback",
+    //             "title": "Yes!",
+    //             "payload": "yes",
+    //           },
+    //           {
+    //             "type": "postback",
+    //             "title": "No!",
+    //             "payload": "no",
+    //           }
+    //         ],
+    //       }]
+    //     }
+    //   }
+    // }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
@@ -119,34 +150,54 @@ function handleMessage(sender_psid, received_message) {
         "payload": {
           "template_type": "generic",
           "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
+            "title": "First card",
+            "subtitle": "Element #1 of an hscroll",
+            "image_url": "https://s-media-cache-ak0.pinimg.com/236x/f6/08/14/f60814c8138ae3529443fd647486065a--tim-walker-photography-mongolia.jpg",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.messenger.com",
+              "title": "web url",
+              // "webview_height_ratio": "tall",
+              // "messenger_extensions": true,
+            }, {
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for first element in a generic bubble",
+            }],
+          }, {
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "https://s-media-cache-ak0.pinimg.com/236x/f6/08/14/f60814c8138ae3529443fd647486065a--tim-walker-photography-mongolia.jpg",
+            "buttons": [{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for second element in a generic bubble",
+            }
+            ],
+          }, {
+            "title": "Third card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "https://s-media-cache-ak0.pinimg.com/236x/f6/08/14/f60814c8138ae3529443fd647486065a--tim-walker-photography-mongolia.jpg",
+            "buttons": [{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for second element in a generic bubble",
+            }
             ],
           }]
         }
       }
     }
-  } 
-  
+
+  }
+
   // Send the response message
-  callSendAPI(sender_psid, response);    
+  callSendAPI(sender_psid, response);
 }
 
 function handlePostback(sender_psid, received_postback) {
   console.log('ok')
-   let response;
+  let response;
   // Get the payload for the postback
   let payload = received_postback.payload;
 
@@ -181,5 +232,5 @@ function callSendAPI(sender_psid, response) {
     } else {
       console.error("Unable to send message:" + err);
     }
-  }); 
+  });
 }
